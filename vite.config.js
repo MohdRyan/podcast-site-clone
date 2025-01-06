f07aaabda35d556a0b72e98104898331d1ca35b1
Your Name <your-email@example.com>
2025-01-06 15:49:49 +0530

    Describe your changes here

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/podcast-site-clone/",
+  base:"https://mohdryan.github.io/podcast-site-clone/",
   plugins: [react()],
 })
